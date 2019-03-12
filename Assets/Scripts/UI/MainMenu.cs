a08e6bfde1823023cf469bb91972ea7862a4264c
Tomás Rinaldi <tomirinaldi8@gmail.com>
2019-03-12 16:11:12 -0300

    Added a menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Simius;
+using Simius.UI;
+
+namespace Chess
+{
+    public class MainMenu : MenuManager
+    {
+
+        [SerializeField, Scene]
+        private string gameScene = default;
+
+
+        public void OnPlayButtonClick()
+        {
+            SceneManager.LoadScene(gameScene);
+        }
+
+        public void OnQuitButtonClick()
+        {
+            Debug.Log("Quitting game...");
+            Application.Quit();
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Simius.UI
+{
+    public class MenuManager : MonoBehaviour, IReadOnlyList<GameObject>
+    {
+
+        [SerializeField]
+        private int activeIndex = 0;
+
+        [SerializeField]
+        private GameObject[] menus = default;
+
+        public int Count => menus.Length;
+        public GameObject this[int index] => menus[index];
+
+
+        private void Start()
+        {
+            UpdateActive();
+        }
+
+
+        private void UpdateActive() => SelectMenu(activeIndex);
+
+
+        public void SelectMenu(int index) => SelectMenu(GetMenu(index));
+
+        public void SelectMenu(GameObject selectMenu)
+        {
+            foreach (GameObject menu in menus)
+            {
+                menu.SetActive(menu == selectMenu);
+            }
+        }
+
+
+        public GameObject GetMenu(int index)
+        {
+            return (menus != null && index >= 0 && index < menus.Length) ? menus[index] : null;
+        }
+
+
+        private void OnValidate()
+        {
+            UpdateActive();
+        }
+
+
+        public IEnumerator<GameObject> GetEnumerator() => (IEnumerator<GameObject>)menus.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => menus.GetEnumerator();
+    }
+}
