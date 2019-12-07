08a00b66cc61d57b93f9df553d7b3a3cfd4ede86
Tomás Rinaldi <tomirinaldi8@gmail.com>
2019-12-07 13:19:35 -0300

    AI can calculate the minmax score

--- Assets/Scripts/Model/Player/ChessMinMaxAI.cs
+++ Assets/Scripts/Model/Player/ChessMinMaxAI.cs
@@ -6,7 +6,7 @@ namespace Chess.Player.AI
 {
     public class ChessMinMaxAI : ChessAI
     {
-        public ChessMinMaxAI(ChessGame game, PieceTeam team) : base(game, team)
+        public ChessMinMaxAI(ChessGame game, PieceTeam team, int maxTurnsDepth = 10) : base(game, team)
         {
         }
 
@@ -15,6 +15,35 @@ namespace Chess.Player.AI
         {
             BoardMovement[] movements = Game.CurrentTurn.AllPossibleMovements.Keys.ToArray();
             DoMovement(movements[RNG.Next(movements.Length)]);
+        }
+
+
+        public int GetPieceValue(Piece piece)
+        {
+            int absoluteValue;
+            switch (piece)
+            {
+                case PiecePawn _:
+                    absoluteValue = 1;
+                    break;
+                case PieceBishop _:
+                case PieceRook _:
+                    absoluteValue = 5;
+                    break;
+                case PieceKnight _:
+                    absoluteValue = 10;
+                    break;
+                case PieceQueen _:
+                    absoluteValue = 20;
+                    break;
+                case PieceKing _:
+                    absoluteValue = 1000;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            };
+
+            return (piece.Team == Team) ? absoluteValue : -absoluteValue;
         }
     }
 }
